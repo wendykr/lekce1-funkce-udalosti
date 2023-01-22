eff090f70d08884ca7067d27da4c49ae480dc3c2
Vendula Krajíčková <vendysacek@seznam.cz>
2023-01-22 17:59:45 +0100

    Hotový úkol

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+// změna barvy na KAŽDÉ kliknutí
+let actualColor = true;
+function changeColor() {
+  let barva = document.querySelector('.ctverecek');
+
+  if(actualColor == true) {
+    barva.style.backgroundColor = 'green';
+    actualColor = false;
+  } else {
+    barva.style.backgroundColor = 'yellow';
+    actualColor = true;
+  }
+}
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
@@ -13,9 +25,33 @@ function secti(a, b) {
   return c;
 }
 
+// ukaž výsledek v prvku ID na KAŽDÉ kliknutí
+let zobrazit = true;
+function vypocitej() {
+  document.querySelector("#vysledek");
+
+  if(zobrazit == true) {
+    document.querySelector("#vysledek").innerHTML = secti(4, 5);
+    zobrazit = false;
+  } else {
+    document.querySelector("#vysledek").innerHTML = '';
+    zobrazit = true;
+  }
+}
+
 /**
  * Upozorní uživatele při spuštění.
  */
-function upozorni() {
+/*function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+}*/
+
+function upozorni() {
+  let myAlert = "Gratulace, právě jsi spustila tuto funkci!";
+
+  let text = document.querySelector('.ctverecek');
+  text.textContent = myAlert;
+  
+  alert(myAlert);
+  console.log(myAlert);
 }
